cdd4aacbeb268809b4ba6da42fda8499d377bb88
홍재성 <126174649+hongjaeseong@users.noreply.github.com>
2024-06-05 08:05:45 +0900

    Add files via upload

---
+++ 2024-06-05/디바이스드라이버구현_홍재성_2024-06-05/WindowsFormsApp1_홍재성/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//추가
+using System.Net.Http;
+using System.Net;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void conn_btn_Click(object sender, EventArgs e)
+        {
+            
+            
+            String port =  this.comboBox1.Items[  this.comboBox1.SelectedIndex  ].ToString();
+            Console.WriteLine("PORT : " + port);
+            HttpWebRequest request=null;
+            HttpWebResponse response = null;
+            try
+            {   
+                request =  (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/connection/" + port);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.Timeout = 30 * 1000;
+
+                response = (HttpWebResponse)request.GetResponse();
+           
+                if(response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine("RESPONSE CODE : " + response.StatusCode);
+
+                }
+                textBox1_TextChanged(sender, e);
+                textBox2_TextChanged(sender, e);
+                textBox3_TextChanged(sender, e);
+                textBox4_TextChanged(sender, e);
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Ex : " + ex);
+            }
+
+        }
+
+        private void led_on_btn_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/led/1");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+
+        private void led_off_btn_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/led/0");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/message/led");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
+            String responseText = reader.ReadToEnd();
+            textBox1.Text = responseText;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/message/tmp");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
+            String responseText = reader.ReadToEnd();
+            textBox2.Text = responseText;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/message/light");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
+            String responseText = reader.ReadToEnd();
+            textBox3.Text = responseText;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/message/distance");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30 * 1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
+            String responseText = reader.ReadToEnd();
+            textBox4.Text = responseText;
+        }
+    }
+}
